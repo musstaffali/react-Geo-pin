a7a006a5f96f837f72069da1faae3ae1d6f288fb
musstaffali <mamustafa@gtcc.edu>
2021-10-11 20:24:37 -0400

    bug fixed

--- src/components/NewMarker.js
+++ src/components/NewMarker.js
@@ -16,8 +16,7 @@ export default function NewMarker({ markLocations, setMarkLocation, latRef, lonR
 					const newEntry = {
 						coords: { lat: parseFloat(e.target.lat.value), lng: parseFloat(e.target.lon.value) },
 						title: e.target.title.value,
-						num: e.target.number.value,
-						add: e.target.address.value,
+						desc: e.target.description.value,
 						img: e.target.image.value
 					};
 
@@ -28,10 +27,9 @@ export default function NewMarker({ markLocations, setMarkLocation, latRef, lonR
 					document.getElementById('newMarker').reset();
 				}}
 			>
-				<input type="text" name="title" autoComplete="off" placeholder="Garden name" required />
-				<input type="text" name="number" autoComplete="off" placeholder="number" />
-				<input type="text" name="address" autoComplete="off" placeholder="address" />
+				<input type="text" name="title" autoComplete="off" placeholder="Title" required />
 				<input type="text" name="image" autoComplete="off" placeholder="Image" />
+				<input type="text" name="description" autoComplete="off" placeholder="Description" />
 				<input type="text" ref={latRef} name="lat" placeholder="Latitude" required />
 				<input type="text" ref={lonRef} name="lon" placeholder="Longitude" required />
 				<Button variant="contained" type="submit" color="primary">
